0729372e1867614056ccb46ac806341c4e20e01c
Rubleenkaur1313 <rubleenkaur05@gmail.com>
2025-02-14 13:11:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-const inputs = document.querySelectorAll('.code');
-
-inputs.forEach((input, index) => {
+document.querySelectorAll('.code').forEach((input, index) => {
     input.addEventListener('input', (e) => {
-        if (e.target.value.length === 1 && index < inputs.length - 1) {
-            inputs[index + 1].focus();
+        // Move to the next input field if a digit is entered
+        if (e.target.value.length === 1 && index < 5) {
+            document.querySelectorAll('.code')[index + 1].focus();
         }
     });
 
     input.addEventListener('keydown', (e) => {
-        if (e.key === 'Backspace' && index > 0 && input.value === '') {
-            inputs[index - 1].focus();
+        // Handle backspace key
+        if (e.key === 'Backspace') {
+            if (input.value.length === 0 && index > 0) {
+                document.querySelectorAll('.code')[index - 1].focus();
+            }
         }
     });
 });
